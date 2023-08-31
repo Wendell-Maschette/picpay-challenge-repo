8e0f49cdeea65e2c65ffa8b101fbcb9e31a26c5a
Wendell Maschette <wendell.maschette@gmail.com>
2023-08-31 14:44:17 -0300

    style: Add Angular Material e json-server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from './material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -8,6 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    ReactiveFormsModule,
+    HttpClientModule,
+    MaterialModule,
     BrowserModule
   ],
   providers: [],
