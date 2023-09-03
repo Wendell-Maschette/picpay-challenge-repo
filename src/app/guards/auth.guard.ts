65a0998c08042664697f2d0563bdb6b9e1cc3324
Wendell Maschette <wendell.maschette@gmail.com>
2023-09-03 03:13:06 -0300

    feat: cria guard de rota para acesso ao dashboard

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,14 @@
+import { CanActivateFn } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
+
+export function authGuard(): CanActivateFn {
+  return () => {
+    const authService: AuthService = inject(AuthService);
+    
+    if (authService.isAuthenticatedUser() ) {
+      return true;
+    }
+    return false;
+  };
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,18 +9,37 @@ import { Account } from '../models/account.interface';
   providedIn: 'root'
 })
 export class AuthService {
-  private dbUrl = 'http://localhost:3030'; 
+  private dbUrl = 'http://localhost:3030';
+  private isAuthenticated = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   login(enteredAccount: Account): Observable<boolean> {
-    return this.http.get<any[]>(`${this.dbUrl}/account`).pipe(
-      map(accounts => {
-        const account = accounts.find(accountAlready => accountAlready.email === enteredAccount.email && accountAlready.password === enteredAccount.password);
-        return !!account; 
+    return this.http.get<Account[]>(`${this.dbUrl}/account`).pipe(
+      map((accounts) => {
+        const account = accounts.find(
+          (accountAlready) =>
+            accountAlready.email === enteredAccount.email &&
+            accountAlready.password === enteredAccount.password
+        );
+
+        !!account ? this.isAuthenticated = true : this.isAuthenticated = false;
+
+        return !!account;
       }),
-      catchError(() => of(false))
+      catchError(() => {
+        this.isAuthenticated = false;
+        return of(false);
+      })
     );
+  }
+
+  logout(): void {
+    this.isAuthenticated = false;
+  }
+
+  isAuthenticatedUser(): boolean {
+    return this.isAuthenticated;
   }
 
   registerAccount(newAccount: Account) {
