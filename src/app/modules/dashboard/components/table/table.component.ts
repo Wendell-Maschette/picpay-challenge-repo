3185e685906154cb5248a8fee565696ca3f52910
Wendell Maschette <wendell.maschette@gmail.com>
2024-02-02 09:34:17 -0300

    feat: ajusta validatore remove botão em duplicidade

--- src/app/modules/dashboard/components/table/table.component.ts
+++ src/app/modules/dashboard/components/table/table.component.ts
@@ -5,21 +5,42 @@ import { Task } from 'src/app/modules/dashboard/models/task.interface';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  styleUrls: ['./table.component.scss'],
 })
 export class TableComponent {
   @Input() dataSource!: MatTableDataSource<Task>;
-  @Input() displayedColumns: string[] = ['name', 'title', 'value', 'date', 'isPayed', 'actions', 'actions-mobile'];
+  @Input() displayedColumns: string[] = [
+    'name',
+    'title',
+    'value',
+    'date',
+    'isPayed',
+    'actions',
+    'actions-mobile',
+  ];
 
   columnHeaders: { [key: string]: string } = {
-    'name': 'Nome',
-    'title': 'Título',
-    'value': 'Valor',
-    'date': 'Data',
-    'isPayed': 'Pago',
-    'actions': 'Ações',
-    'actions-mobile': 'Ações'
+    name: 'Nome',
+    title: 'Título',
+    value: 'Valor',
+    date: 'Data',
+    isPayed: 'Pago',
+    actions: 'Ações',
+    'actions-mobile': 'Ações',
   };
+
+  defaultColumValidator(columnParam: string): boolean {
+    if (
+      columnParam !== 'actions' &&
+      columnParam !== 'actions-mobile' &&
+      columnParam !== 'date' &&
+      columnParam !== 'value' &&
+      columnParam !== 'isPayed'
+    ) {
+      return true;
+    }
+    return false
+  }
 
   editItem(index: number) {
     // Lógica para editar item
